3be0fceb30fa90177b24379f584876c97f5c8e09
Craig Brett <craigbrett17@aol.com>
2022-03-31 19:47:08 +0100

    Initial commit

---
+++ ScreenReaderAccess/Assets/UI/Accessibility/ScreenReader.lua
@@ -0,0 +1,10 @@
+-- ScreenReader module
+--------------------------------------------------------------
+
+local SCREENREADER_LOG_PREFIX: string = "#SCREENREADER - ";
+
+function OutputMessageToScreenReader(message)
+    print("Entering outputMessage");
+	print(output); -- need a way to output this
+	print("Done");
+end
