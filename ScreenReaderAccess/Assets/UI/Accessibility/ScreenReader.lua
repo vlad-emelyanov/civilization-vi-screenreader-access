da96726455183e49fb7abceb1b3c3792f265abcd
Craig Brett <craigbrett17@aol.com>
2022-03-31 19:49:09 +0100

    Change to outputting formatted log message to logs
    Used for proof of concept

--- ScreenReaderAccess/Assets/UI/Accessibility/ScreenReader.lua
+++ ScreenReaderAccess/Assets/UI/Accessibility/ScreenReader.lua
@@ -5,6 +5,6 @@ local SCREENREADER_LOG_PREFIX: string = "#SCREENREADER - ";
 
 function OutputMessageToScreenReader(message)
     print("Entering outputMessage");
-	print(output); -- need a way to output this
+	print(SCREENREADER_LOG_PREFIX .. message);
 	print("Done");
 end
