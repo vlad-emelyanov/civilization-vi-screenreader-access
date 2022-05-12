1c3a3cbab49db68b5c50182c3e7623b6611e9015
Craig Brett <craigbrett17@aol.com>
2022-05-13 00:57:25 +0100

    Read out adjacent units and cities on unit selection change

--- ScreenReaderAccess/Assets/UI/Accessibility/ScreenReaderEventHandlers.lua
+++ ScreenReaderAccess/Assets/UI/Accessibility/ScreenReaderEventHandlers.lua
@@ -3,6 +3,18 @@
 
 include("ScreenReader");
 
+-- locals
+
+-- used to get the suffix for a direction for getting the localized string version from a direction
+local DIRECTION_MAP = {
+	[DirectionTypes.DIRECTION_NORTHEAST] = "NORTH_EAST",
+	[DirectionTypes.DIRECTION_EAST] = "EAST",
+	[DirectionTypes.DIRECTION_SOUTHEAST] = "SOUTH_EAST",
+	[DirectionTypes.DIRECTION_NORTHWEST] = "NORTH_WEST",
+	[DirectionTypes.DIRECTION_WEST] = "WEST",
+	[DirectionTypes.DIRECTION_SOUTHWEST] = "SOUTH_WEST"
+}
+
 -- ===========================================================================
 --	Game Engine Event
 -- ===========================================================================
@@ -15,6 +27,12 @@ function OnUnitSelectionChanged( playerID:number, unitID:number, hexI:number, he
 	local tUnitData = GameInfo.Units[pUnit:GetType()]
 	local name = Locale.Lookup(tUnitData.Name);
 	OutputMessageToScreenReader(name);
+
+	local iPois = GetAdjacentPointsOfInterestFrom(hexI, hexJ);
+	local poisString = TurnPointsOfInterestIntoString(iPois);
+	if poisString ~= nil then
+		OutputMessageToScreenReader(poisString);
+	end
 end
 
 -- ===========================================================================
@@ -33,9 +51,72 @@ function OnCitySelectionChanged( owner:number, cityID:number, i, j, k, isSelecte
 	OutputMessageToScreenReader(name .. additionalInfo);
 end
 
+function GetAdjacentPointsOfInterestFrom(hexX: number, hexY: number)
+	local units = {}
+	local cities = {}
+
+	for direction = 0, DirectionTypes.NUM_DIRECTION_TYPES - 1, 1 do
+		local adjacentPlot = Map.GetAdjacentPlot(hexX, hexY, direction);
+		local iX, iY = adjacentPlot:GetX(), adjacentPlot:GetY();
+
+		if adjacentPlot then
+			for i, unit in ipairs(Units.GetUnitsInPlot(adjacentPlot)) do
+				table.insert(units, { unit = unit, direction = direction });
+			end
+
+			if Cities.GetCityInPlot(adjacentPlot) ~= nil then
+				table.insert(cities, { city = Cities.GetCityInPlot(adjacentPlot), direction = direction });
+			end
+		end
+	end
+
+	return { units = units, cities = cities };
+end
+
+function TurnPointsOfInterestIntoString(iPois: table)
+	local output = {}
+
+	for i, item in ipairs(iPois.units) do
+		local owner = item.unit:GetOwner();
+		local adjective = PlayerConfigurations[owner]:GetCivilizationDescription();
+		local stringified = Locale.Lookup(adjective) ..
+			" " .. Locale.Lookup(item.unit:GetName()) ..
+			" " .. GetLocalizedDirectionString(item.direction);
+		table.insert(output, stringified);
+	end
+
+	for i, item in ipairs(iPois.cities) do
+		local owner = item.city:GetOwner();
+		local adjective = PlayerConfigurations[owner]:GetCivilizationDescription();
+		local stringified = Locale.Lookup(adjective) ..
+			" " .. Locale.Lookup("LOC_CITY_NAME_BLANK") ..
+			" " .. Locale.Lookup(item.city:GetName()) ..
+			" " .. GetLocalizedDirectionString(item.direction);
+		table.insert(output, stringified);
+	end
+
+	if #output > 0 then
+		return table.concat(output, "[NEWLINE]");
+	else
+		return nil;
+	end
+end
+
+function GetLocalizedDirectionString(direction)
+	local directionTextPrefix = "LOC_DIRECTION_";
+
+	local directionTextSuffix = DIRECTION_MAP[direction];
+	if directionTextSuffix == nil then
+		return "";
+	end
+
+	local localizedDirectionText = Locale.Lookup(directionTextPrefix .. directionTextSuffix);
+	return localizedDirectionText;
+end
+
 function Initialize()
 	print("Initializing screen reader event handlers");
-	
+
 	-- hook into game and lua events to call above methods
 	Events.UnitSelectionChanged.Add( OnUnitSelectionChanged );
 	Events.CitySelectionChanged.Add( OnCitySelectionChanged );
